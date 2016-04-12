d3cbc40b3b519a2718b63a7c513a5c5ad3b1af6e
Kalle Pahajoki <kalle.pahajoki@mystes.fi>
2016-04-12 17:35:04 +0300

    Change format of default bad API key

--- samples/demo/aino-config.sh
+++ samples/demo/aino-config.sh
@@ -2,7 +2,7 @@
 # This file should be customized for each user.
 
 # This is the API key used to communicate with Aino instance. API keys can be managed through the aino.io admin interfaces
-export AINO_API_KEY="<your-api-key-here>"
+export AINO_API_KEY="ENTER YOUR AINO API KEY HERE"
 
 # Other variables that can be set here: IMPLEMENTATION, AINO_URL
 #
