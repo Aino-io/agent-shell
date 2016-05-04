bfbe16044e5040df2c34f739fd24f2d900037bf0
Kalle Pahajoki <kalle.pahajoki@gmail.com>
2016-05-04 14:43:12 +0300

    More robust parsing of HTTP status code for wget (#9)
    * Implement support for metadata
    
    * Do not use non-verbose mode because wget 1.9.1 doesn't print HTTP status code if used. More robust parsing of response code

--- scripts/send_wget.sh
+++ scripts/send_wget.sh
@@ -27,14 +27,14 @@ fi
 	if [ "$AINO_HTTP_OUT" != "" ]; then
 	    OUT=$AINO_HTTP_OUT
 	fi
-    OUTPUT="`wget -nv --server-response --header=\"Authorization: apikey ${AINO_API_KEY}\" --header=\"Content-type: application/json\" ${AINO_URL} --post-data \"$2\" -O - 2>&1`"
+    OUTPUT="`wget --server-response --header=\"Authorization: apikey ${AINO_API_KEY}\" --header=\"Content-type: application/json\" ${AINO_URL} --post-data \"$2\" -O - 2>&1`"
 	if [ "${VERBOSE_AINO}" = "true" ]; then
-	    echo "Command: wget -nv --server-response --header=\"Authorization: apikey ${AINO_API_KEY}\" --header=\"Content-type: application/json\" ${AINO_URL} --post-data \"payload\" -O - 2>&1"
+	    echo "Command: wget --server-response --header=\"Authorization: apikey ${AINO_API_KEY}\" --header=\"Content-type: application/json\" ${AINO_URL} --post-data \"payload\" -O - 2>&1"
     	echo "$OUTPUT"
     fi
     echo "$OUTPUT" > $OUT
 
-    STATUSCODE="`echo \"$OUTPUT\"|head -n1|cut -d' ' -f4`"
+    STATUSCODE="`echo \"$OUTPUT\"|grep HTTP/|cut -d'/' -f2|cut -d' ' -f2`"
     ERROR="`echo \"$OUTPUT\"|tail -n1`"
 
 	if [ "$STATUSCODE" != "200" -a "$STATUSCODE" != "202" ]; then
